a7d4a6bbf16f9bb81f79f2d2600c3fa876122d38
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-27 21:52:23 +0200

    refactor: add footer components

---
+++ components/V-Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer">
+    <p class="footer__text">
+      Inspired by
+      <NuxtLink href="https://www.discoveroutpost.com">Outpost</NuxtLink>
+    </p>
+  </footer>
+</template>
+
+<style lang="scss">
+.footer {
+  width: 90%;
+  max-width: 75ch;
+
+  margin: 0 auto;
+  padding: 1rem 2rem;
+
+  &__text {
+    font-size: 1.5rem;
+    text-align: center;
+
+    a {
+      color: currentColor;
+    }
+  }
+}
+</style>
