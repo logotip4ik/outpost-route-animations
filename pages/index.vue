f2108e0c18552524bd4a81e32bde304c9a418be9
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-27 16:05:56 +0200

    refactor: move styles from attributes to class

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import '@master/css';
-
 const { data: routes } = useFetch('/api/work');
 
 function generateWorkPagePath(workName: string) {
@@ -10,43 +8,22 @@ function generateWorkPagePath(workName: string) {
 </script>
 
 <template>
-  <div pt-64 pb-32 max-w-85ch w-90% m-auto>
-    <header mb-48 op-90>
-      <h1 text-size-6xl>Outpost inspired animations</h1>
+  <div class="pt-64 pb-32 max-w-85ch w-90% m-auto">
+    <header class="mb-48 op-90">
+      <h1 class="text-size-6xl">Outpost inspired animations</h1>
     </header>
 
     <main>
-      <h2 pl-4 text-size-5xl sm:pl-8>Works</h2>
+      <h2 class="pl-4 text-size-5xl sm:pl-8">Works</h2>
 
-      <ul list-none m-0 p-0>
-        <li v-for="route in routes" :key="route.name" mb-12rem>
+      <ul class="list-none m-0 p-0">
+        <li v-for="route in routes" :key="route.name" class="mb-12rem">
           <NuxtLink
             :to="generateWorkPagePath(route.name)"
-            flex
-            flex-col-reverse
-            justify-center
-            content-end
-            w-full
-            decoration-none
-            text-current
-            op-85
-            hover:op-100
-            transition
-            sm:flex-row
+            class="flex flex-col-reverse justify-center content-end w-full decoration-none text-current op-85 hover:op-100 transition sm:flex-row"
           >
             <p
-              text-center
-              block
-              m-0
-              h-min
-              uppercase
-              text-size-4xl
-              sm:(
-              mt-auto
-              mb--1.5
-              mr-5
-              w-min
-              text-right)
+              class="text-center block m-0 h-min uppercase text-size-4xl sm:(mt-auto mb--1.5 mr-5 w-min text-right)"
             >
               {{ route.name }}
             </p>
@@ -58,11 +35,7 @@ function generateWorkPagePath(workName: string) {
               :height="route.image.height"
               :alt="`Photo by ${route.name}`"
               loading="lazy"
-              block
-              w-full
-              max-h-24rem
-              object-cover
-              sm:(max-w-50%)
+              class="block w-full max-h-24rem object-cover sm:(max-w-50%)"
             />
           </NuxtLink>
         </li>
