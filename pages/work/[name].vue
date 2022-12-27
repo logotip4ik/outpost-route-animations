78f0f8128739d858aeb5eeaa065f89b40efa9bde
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-27 16:05:41 +0200

    feat: style work page

--- pages/work/[name].vue
+++ pages/work/[name].vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
 const route = useRoute();
-const { data: work } = useFetch(`/api/work/${route.params.name}`);
+
+const { data: work, error } = await useFetch(`/api/work/${route.params.name}`);
+
+watch(error, (error) => {
+  if (!error) return;
+
+  showError(error);
+});
 </script>
 
 <template>
-  <main>
-    {{ work }}
-  </main>
+  <div class="pt-64 pb-32 max-w-85ch w-90% m-auto">
+    <header class="flex flex-col justify-between content-start mb-36 op-90">
+      <h1 class="text-size-6xl mb-10rem">{{ work!.name }}</h1>
+
+      <img
+        :src="work!.image.src"
+        :width="work!.image.width"
+        :height="work!.image.height"
+        :alt="`Photo by ${work!.name}`"
+        class="self-end w-full max-w-65vw max-h-65vw object-cover sm:(max-w-50vw max-h-50vw) xl:(max-w-40rem max-h-40rem)"
+      />
+    </header>
+
+    <main>
+      <p v-for="text in work!.text" class="text-size-xl mb-24">
+        {{ text }}
+      </p>
+    </main>
+  </div>
 </template>
